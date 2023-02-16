e9eb60a536f7537d0ac1d910e6e1205b9cf71954
Rosângela Nery <rosangela2nery@gmail.com>
2023-02-16 20:33:17 -0300

    Exchanging questions for world knowledge

--- src/Primeiro_Card/CardVirado1.js
+++ src/Primeiro_Card/CardVirado1.js
@@ -9,43 +9,43 @@ export default function CardVirado1 ({
     let itens = [
         {
             title: "Pergunta",
-            pergunta: "O que é JSX?",
-            resposta: "Uma extensão de linguagem do JavaScripts",
+            pergunta: "Quem foi a primeira pessoa a viajar no Espaço?",
+            resposta: "O russo Yuri Gagarin (1934-1968) foi a primeira pessoa a viajar para o espaço, o que aconteceu em 12 de abril de 1961.",
         },
         {
             title: "Pergunta",
-            pergunta: "O que é React?",
-            resposta: "Uma biblioteca JavaScript para construção de interfaces",
+            pergunta: "Qual a montanha mais alta do mundo?",
+            resposta: "O Monte Everest tem 8.848 metros de altitude e localiza-se no Nepal, um país asiático que faz fronteira com a China e com a Índia.",
         },
         {
             title: "Pergunta",
-            pergunta: " Componentes devem iniciar com __ ",
-            resposta: " A letra maiúscula",
+            pergunta: "Onde se localiza Machu Picchu?",
+            resposta: "Machu Picchu localiza-se no Peru, país da América do Sul que faz fronteira com o Brasil.",
         },
         {
             title: "Pergunta",
-            pergunta: "Podemos colocar __ dentro do JSX",
-            resposta: " Expressões",
+            pergunta: "Que país tem o formato de uma bota?",
+            resposta: "No mapa da Europa, a Itália se destaca por parecer uma bota de cano e salto altos.",
         },
         {
             title: "Pergunta",
-            pergunta: "O ReactDOM nos ajuda __ ",
-            resposta: " Interagindo com a DOM para colocar componentes React na mesma",
+            pergunta: "Quem inventou a lâmpada?",
+            resposta: "A lâmpada foi inventada por Thomas Edison (1847-1931) em 1879. No dia 21 de outubro, o inventor conseguiu manter uma lâmpada acessa durante 48 horas.",
         },
         {
             title: "Pergunta",
-            pergunta: "Usamos o npm para __",
-            resposta: "gerenciar os pacotes necessários e suas dependências",
+            pergunta: "Quanto tempo a Terra demora para dar uma volta completa em torno dela mesma?",
+            resposta: "A Terra demora aproximadamente 24 horas, mais precisamente 23 horas, 56 minutos e 4 segundos para dar uma volta completa em torno do seu próprio eixo. Esse movimento recebe o nome de rotação.",
         },
         {
             title: "Pergunta",
-            pergunta: "Usamos props para __",
-            resposta: "passar diferentes informações para componentes ",
+            pergunta: "Quais são as fases da Lua?",
+            resposta: "As fases da lua são quatro: nova, crescente, cheia e minguante. Cada uma delas dura 7 ou 8 dias.",
         },
         {
             title: "Pergunta",
-            pergunta: "Usamos estado (state) para __",
-            resposta: "dizer para o React quais informações quando atualizadas devem renderizar a tela novamente",
+            pergunta: "Qual o maior planeta do sistema solar?",
+            resposta: "Júpiter é 1300 vezes maior que a Terra, sendo o maior planeta do sistema solar.",
         }
     ]
 
